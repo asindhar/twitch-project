d1fff0ec8534ad80352079b214fdbde6c2da486a
aman <aman.s.s@outlook.com>
2019-02-01 18:20:19 -0800

    api url on production

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,3 @@
-exports.API_URL = 'http://localhost:8080'
+export const API_URL = process.env.NODE_ENV === 'production' 
+  ? 'https://fav-streamer-event-viewer.herokuapp.com'
+  : 'https://localhost:8080'
