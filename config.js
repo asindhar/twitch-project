54515d2388d99df062bdf3dee04b49d79fa66789
aman <aman.s.s@outlook.com>
2019-02-01 20:56:02 -0800

    callback url changed

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
 
 exports.CLIENT_ORIGIN = ['http://127.0.0.1:3000', 'http://localhost:3000']
- 
+
+const CALLBACK_URL = process.env.NODE_ENV === 'production' 
+  ? 'https://fav-streamer-event-viewer.herokuapp.com/twitch/callback'
+  : 'http://localhost:8080/twitch/callback'
+
 exports.TWITCH_CONFIG = {
     clientID: process.env.TWITCH_CLIENT_ID,
     clientSecret: process.env.TWITCH_CLIENT_SECRET,
-    callbackURL: 'http://localhost:8080/twitch/callback',
+    callbackURL: CALLBACK_URL,
     scope: "user_read chat:edit"
 }
