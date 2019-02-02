20fdfa5de7caa3255ea0fb14964d9a0b571e6131
aman <aman.s.s@outlook.com>
2019-02-01 20:32:11 -0800

    testing

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://fav-streamer-event-viewer.herokuapp.com/twitch'
+  ? 'https://fav-streamer-event-viewer.herokuapp.com'
   : 'https://localhost:8080'
